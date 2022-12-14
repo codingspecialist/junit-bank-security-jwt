840d52fe6ebef225a3d5ecdca2ccedde3265e899
ssar <ssar@nate.com>
2022-12-14 11:51:41 +0900

    계좌등록 테스트 중

--- src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
+++ src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
@@ -1,19 +1,58 @@
 package shop.mtcoding.bank.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
+import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 
 @ExtendWith(MockitoExtension.class)
 public class AccountServiceTest extends DummyObject {
+    private final Logger log = LoggerFactory.getLogger(getClass());
     @InjectMocks
     private AccountService accountService;
 
     @Mock
     private AccountRepository accountRepository;
+
+    @Spy
+    private ObjectMapper om;
+
+    @Test
+    public void 계좌등록_test() throws Exception {
+        // given
+        AccountSaveReqDto accountSaveReqDto = new AccountSaveReqDto();
+        accountSaveReqDto.setNumber(1111L);
+        accountSaveReqDto.setPassword("1234");
+
+        // stub
+        User ssar = newMockUser(1L, "ssar", "쌀");
+        Account ssarAccount = newMockAccount(1L, 1111L, ssar);
+        when(accountRepository.save(any())).thenReturn(ssarAccount);
+
+        // when
+        AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, ssar);
+        String responseBody = om.writeValueAsString(accountSaveRespDto);
+        log.debug("테스트 : " + responseBody);
+
+        // then
+        assertThat(accountSaveRespDto.getNumber()).isEqualTo(1111L);
+    }
 
 }
