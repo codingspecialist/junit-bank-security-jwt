c6967f0ef712ef66f0015540994c2ba45535039f
ssar <ssar@nate.com>
2022-12-14 14:56:45 +0900

    계좌등록 서비스 테스트 완료

--- src/main/java/shop/mtcoding/bank/service/AccountService.java
+++ src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -7,18 +7,23 @@ import lombok.RequiredArgsConstructor;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class AccountService {
+    private final UserRepository userRepository;
     private final AccountRepository accountRepository;
 
     @Transactional
-    public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, User user) {
-        Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(user));
+    public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다"));
+        Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
         return new AccountSaveRespDto(accountPS);
     }
 }
--- src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
+++ src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,6 +21,7 @@ import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 
@@ -31,6 +34,9 @@ public class AccountServiceTest extends DummyObject {
     @Mock
     private AccountRepository accountRepository;
 
+    @Mock
+    private UserRepository userRepository;
+
     @Spy
     private ObjectMapper om;
 
@@ -41,13 +47,16 @@ public class AccountServiceTest extends DummyObject {
         accountSaveReqDto.setNumber(1111L);
         accountSaveReqDto.setPassword("1234");
 
-        // stub
+        // stub 1
         User ssar = newMockUser(1L, "ssar", "쌀");
+        when(userRepository.findById(ssar.getId())).thenReturn(Optional.of(ssar));
+
+        // stub 2
         Account ssarAccount = newMockAccount(1L, 1111L, ssar);
         when(accountRepository.save(any())).thenReturn(ssarAccount);
 
         // when
-        AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, ssar);
+        AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, ssar.getId());
         String responseBody = om.writeValueAsString(accountSaveRespDto);
         log.debug("테스트 : " + responseBody);
 
