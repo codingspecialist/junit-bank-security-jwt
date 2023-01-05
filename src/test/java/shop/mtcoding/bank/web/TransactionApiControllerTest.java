b9f8991eb5856405ff5e577c8c45d7c1efb80f56
metacoding <getinthere@naver.com>
2023-01-05 22:06:35 +0900

    입출금 목록보기 테스트 완료

---
+++ src/test/java/shop/mtcoding/bank/web/TransactionApiControllerTest.java
@@ -0,0 +1,93 @@
+package shop.mtcoding.bank.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.domain.transaction.TransactionRepository;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+
+@ActiveProfiles("test")
+@Sql("classpath:db/teardown.sql") // teardown
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class TransactionApiControllerTest extends DummyObject {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private static final String APPLICATION_JSON_UTF8 = "application/json; charset=utf-8";
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @BeforeEach
+    public void setUp() {
+        User ssar = userRepository.save(newUser("ssar", "쌀"));
+        User cos = userRepository.save(newUser("cos", "코스,"));
+        User love = userRepository.save(newUser("love", "러브"));
+        User admin = userRepository.save(newUser("admin", "관리자"));
+        Account ssarAccount1 = accountRepository.save(newAccount(1111L, ssar));
+        Account cosAccount = accountRepository.save(newAccount(2222L, cos));
+        Account loveAccount = accountRepository.save(newAccount(3333L, love));
+        Account ssarAccount2 = accountRepository.save(newAccount(4444L, ssar));
+        Transaction withdrawTransaction1 = transactionRepository
+                .save(newWithdrawTransaction(ssarAccount1));
+        Transaction depositTransaction1 = transactionRepository
+                .save(newDepositTransaction(cosAccount));
+        Transaction transferTransaction1 = transactionRepository
+                .save(newTransferTransaction(ssarAccount1, cosAccount));
+        Transaction transferTransaction2 = transactionRepository
+                .save(newTransferTransaction(ssarAccount1, loveAccount));
+        Transaction transferTransaction3 = transactionRepository
+                .save(newTransferTransaction(cosAccount, ssarAccount1));
+    }
+
+    // 계좌 비밀번호 BCrypt 인코딩 (숙제)
+    @WithUserDetails(value = "ssar", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    public void findTransactionList_test() throws Exception {
+        // given
+        Long accountNumber = 1111L;
+        String gubun = "ALL";
+        String page = "0";
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(get("/api/s/account/" + accountNumber + "/transaction").param("gubun", gubun).param("page",
+                        page));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        log.debug("디버그 : " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isOk());
+
+    }
+
+}
